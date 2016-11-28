993e58182170300537178c6acd0dfd1908d0890c
denis <denis_lton@hotmail.com>
2016-11-28 10:08:27 +0000

    add manacer O(n) method

--- algorithms/5.longestPalindromicSubstring.py
+++ algorithms/5.longestPalindromicSubstring.py
@@ -41,6 +41,7 @@ class Solution(object):
             return s
 
         maxLen, start = (0, 0)
+
         for i in range(1, ls):
             #  center as i, expanding as even palindrom
             low, high = (i - 1, i)
@@ -62,8 +63,71 @@ class Solution(object):
 
         return s[start:(maxLen+start)]
 
+    def longestPalindromicSubstring_opt_center(self, s):
+        """
+        Center by one element and calulate each palindromic substring by
+        expanding the letters
+        """
+        ls = len(s)
+        if ls == 1:
+            return s
+
+        start, end = (0, 0)
+
+        def expand_around_center(s, low, high):
+
+            while low >= 0 and high < ls and s[low] == s[high]:
+                low -= 1
+                high += 1
+            return high - low - 1
+
+        for i in range(1, ls):
+            len1 = expand_around_center(s, i, i)
+            len2 = expand_around_center(s, i, i+1)
+            ml = max(len1, len2)
+            if ml > end - start:
+                start = i - (ml - 1) // 2
+                end = i + ml // 2
+        return s[start:end+1]
+
     def longestPalindromicSubstring_manacher(self, s):
-        pass
+        #  refer to http://www.cnblogs.com/TenosDoIt/p/3675788.html
+        #  idx as current longest palindrom string center
+        #  mx as current longest palindrom string most right position
+        def pre_process(s):
+            rs = '^'
+            for se in s:
+                rs += '#' + se
+            rs += '#$'
+            return rs
+
+        if len(s) == 1:
+            return s
+        ss = pre_process(s)
+        ls, idx, mx = (len(ss), 0, 0)
+
+        print(ss)
+        # initilize the point array
+        p = [0] * ls
+        for i in range(1, ls-1):
+            p[i] = min(p[2*idx-i], p[mx-i]) if mx > i else 1
+            print(p)
+            while ss[i+p[i]] == ss[i-p[i]]:
+                p[i] += 1
+            if i + p[i] > mx:
+                mx = i + p[i]
+                idx = i
+
+        maxLen, index = (0, 0)
+        for i in range(1, ls-1):
+            if p[i] > maxLen:
+                maxLen = p[i]
+                index = i
+
+        #  print("{0}, {1}".format(maxLen, index))
+        start = (index - maxLen) // 2
+        end = start + maxLen - 1
+        return s[start:end]
 
 
 if __name__ == "__main__":
@@ -76,3 +140,11 @@ if __name__ == "__main__":
     #  print(e.longestPalindromicSubstring_center("babad"))
     #  print(e.longestPalindromicSubstring_center("cbbd"))
     #  print(e.longestPalindromicSubstring_center("abcda"))
+
+    #  print(e.longestPalindromicSubstring_opt_center("babad"))
+    #  print(e.longestPalindromicSubstring_opt_center("cbbd"))
+    #  print(e.longestPalindromicSubstring_opt_center("abcda"))
+
+    print(e.longestPalindromicSubstring_manacher("babad"))
+    print(e.longestPalindromicSubstring_manacher("cbbd"))
+    print(e.longestPalindromicSubstring_manacher("abcda"))
